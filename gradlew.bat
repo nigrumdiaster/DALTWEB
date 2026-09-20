822d01e72f548e1588ad634dd471d7d344a48da5
Dai Nghia <nigrumdiaster@gmail.com>
2026-09-20 17:36:57 +0700

    Update java path

--- gradlew.bat
+++ gradlew.bat
@@ -39,6 +39,12 @@ for %%i in ("%APP_HOME%") do set APP_HOME=%%~fi
 set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
 
 @rem Find java.exe
+if not defined JAVA_HOME (
+    if exist "C:\Users\kuuhaku\.jdks\openjdk-26.0.2.1\bin\java.exe" (
+        set "JAVA_HOME=C:\Users\kuuhaku\.jdks\openjdk-26.0.2.1"
+    )
+)
+
 if defined JAVA_HOME goto findJavaFromJavaHome
 
 set JAVA_EXE=java.exe
@@ -54,9 +60,11 @@ echo location of your Java installation. 1>&2
 "%COMSPEC%" /c exit 1
 
 :findJavaFromJavaHome
-set JAVA_HOME=%JAVA_HOME:"=%
-set JAVA_EXE=%JAVA_HOME%/bin/java.exe
+set "JAVA_EXE=%JAVA_HOME%\bin\java.exe"
 
+if exist "%JAVA_EXE%" goto execute
+
+set "JAVA_EXE=%JAVA_HOME%/bin/java.exe"
 if exist "%JAVA_EXE%" goto execute
 
 echo. 1>&2
